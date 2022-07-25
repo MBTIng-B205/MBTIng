89a8b72c02439c57b8caf595039ad0034af793f5
gun <wmf3362@naver.com>
2022-07-25 10:43:44 +0900

    fe push

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-button @click="onOpenLoginDialog">Login</el-button>
+  <login-dialog
+    :open="loginDialogOpen"
+    @closeLoginDialog="onCloseLoginDialog"
+  />
+</template>
+
+<script>
+import LoginDialog from "./components/login-dialog";
+
+export default {
+  components: {
+    LoginDialog,
+  },
+  data() {
+    return {
+      loginDialogOpen: false,
+    };
+  },
+
+  methods: {
+    onOpenLoginDialog() {
+      this.loginDialogOpen = true;
+    },
+    onCloseLoginDialog() {
+      this.loginDialogOpen = false;
+    },
+  },
+};
+</script>
