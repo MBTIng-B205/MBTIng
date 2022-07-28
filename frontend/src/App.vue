77c1a5d320d7436ffae1d93e4f9b88946ed08334
SSAFY <martin0125>
2022-07-28 13:15:50 +0900

    bg_update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="background">
+    <img src="@/assets/BG_Heart_Bar.gif" id="bg" alt="" />
     <router-view></router-view>
   </div>
 </template>
@@ -18,8 +19,12 @@ export default {
 
 <style>
 .background {
-  height: 1024px;
-  width: 1440px;
-  background-image: url("@/assets/BG_Heart.gif");
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  /* Preserve aspet ratio */
+  min-width: 100%;
+  min-height: 100%;
 }
 </style>
