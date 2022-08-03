99a3f1813d5929aa71572665c20b4779309bb6e9
lye2i <65855978+lye2i@users.noreply.github.com>
2022-08-03 15:32:17 +0900

    front 1주차 삭제
    front 1주차 삭제

--- frontend/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "vuex";
-import { accounts } from "@/store/modules/accounts";
-import { meetings } from "@/store/modules/meetings";
-
-export default createStore({
-  modules: { accounts, meetings },
-});
--- frontend/src/views/MyPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>Mypage</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
