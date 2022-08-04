b29610408f97b4a5af5841f79ca7921438a4571d
Je-yeong <wp29dud@gmail.com>
2022-08-04 23:53:09 +0900

    misc : Add log some env var

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,5 +7,6 @@ import "element-plus/dist/index.css";
 
 console.log(process.env);
 console.log(process.env.VUE_APP_KAKAO_LOGIN_REDIRECT_URI);
+console.log(process.env.VUE_APP_API_SERVER_BASE_URL);
 
 createApp(App).use(ElementPlus).use(store).use(router).mount("#app");
