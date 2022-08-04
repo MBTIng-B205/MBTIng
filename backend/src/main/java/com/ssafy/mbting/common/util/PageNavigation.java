a3089ab5fa2f47625a6ca37fa30897f048406f29
이기진 <rlwls1102@gmail.com>
2022-08-04 17:29:48 +0900

    Update PageNavigation.java

--- backend/src/main/java/com/ssafy/mbting/common/util/PageNavigation.java
+++ backend/src/main/java/com/ssafy/mbting/common/util/PageNavigation.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PageNavigation {
-    int page;
-    int size;
+    Integer page;
+    Integer size;
     SearchUtil searchUtil;
 }
