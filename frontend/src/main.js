130a8f9260259eaad4cea1b1f7e72c0eba1492af
Je-yeong <wp29dud@gmail.com>
2022-08-04 16:52:07 +0900

    feat : Add .env.production

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,4 +5,6 @@ import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
+console.log("인브", process.env);
+
 createApp(App).use(ElementPlus).use(store).use(router).mount("#app");
