c4c2070cf336e0368150d30767f40d537ef06e97
Gungil Bae <97593802+geon-gil@users.noreply.github.com>
2022-08-04 20:02:53 +0900

    FE: MeetingRoom 생성

--- frontend/src/views/MeetingRoom.vue
+++ frontend/src/views/MeetingRoom.vue
@@ -1,11 +1,21 @@
 <template>
-  <footer>
-    <div class="container"></div>
-  </footer>
+  <div class="container">
+    안녕하세요
+    <el-icon>
+      <Opportunity />
+    </el-icon>
+    <el-icon :size="20">
+      <Edit />
+    </el-icon>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  setup() {
+    return {};
+  },
+};
 </script>
 
 <style></style>
