a05591e1716bfe2fd24fd9fa461fb1430314e325
Je-yeong <wp29dud@gmail.com>
2022-08-04 23:05:23 +0900

    fix : Update LoginView for env var

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
-console.log("인브", process.env);
+console.log(process.env);
+console.log(process.env.VUE_APP_KAKAO_LOGIN_REDIRECT_URI);
 
 createApp(App).use(ElementPlus).use(store).use(router).mount("#app");
--- frontend/src/store/modules/accounts.js
+++ frontend/src/store/modules/accounts.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const base = {
-  baseUrl: "http://localhost:8080/api",
+  baseUrl: process.env.VUE_APP_API_SERVER_BASE_URL,
   headers: {
     "Content-type": "application/json",
   },
