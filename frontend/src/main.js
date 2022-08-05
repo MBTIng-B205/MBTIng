ee29c45e192b73a21a6320885abd045482be3f14
송제영 <wp29dud@naver.com>
2022-08-05 11:41:24 +0900

    misc : Remove log

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,4 @@ import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
-console.log(process.env);
-console.log(process.env.VUE_APP_KAKAO_LOGIN_REDIRECT_URI);
-console.log(process.env.VUE_APP_API_SERVER_BASE_URL);
-
 createApp(App).use(ElementPlus).use(store).use(router).mount("#app");
