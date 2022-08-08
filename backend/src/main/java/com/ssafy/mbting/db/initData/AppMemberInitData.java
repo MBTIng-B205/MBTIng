d624e10017666637cdbe42dc9fac1f433838de06
lgj2022 <98953739+lgj2022@users.noreply.github.com>
2022-08-08 15:22:03 +0900

    misc : 줄정리 및 import정리

--- backend/src/main/java/com/ssafy/mbting/db/initData/AppMemberInitData.java
+++ backend/src/main/java/com/ssafy/mbting/db/initData/AppMemberInitData.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
