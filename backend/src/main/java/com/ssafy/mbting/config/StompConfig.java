53517355bf69bf46b11cc4ea1e3337b11a83e5cf
Je-yeong <wp29dud@gmail.com>
2022-08-08 10:45:26 +0900

    feat : Add @EnableAsync to config

--- backend/src/main/java/com/ssafy/mbting/config/StompConfig.java
+++ backend/src/main/java/com/ssafy/mbting/config/StompConfig.java
@@ -2,11 +2,13 @@ package com.ssafy.mbting.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableAsync
 @EnableWebSocketMessageBroker
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
