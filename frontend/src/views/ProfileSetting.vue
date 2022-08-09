03b9c608c566e4229d3a6276c9283da51476cdf9
sc-choi2022 <97648355+sc-choi2022@users.noreply.github.com>
2022-08-09 10:53:58 +0900

    fix: gender radio

--- frontend/src/views/ProfileSetting.vue
+++ frontend/src/views/ProfileSetting.vue
@@ -37,8 +37,8 @@
             </el-form-item>
             <el-form-item label="성별">
               <el-radio-group v-model="form.gender" style="margin-left: 30px">
-                <el-radio :label="true" :value="Male">남자</el-radio>
-                <el-radio :label="false" :value="Female">여자</el-radio>
+                <el-radio label="MALE">남자</el-radio>
+                <el-radio label="FEMALE">여자</el-radio>
               </el-radio-group>
             </el-form-item>
             <el-form-item label="생년월일">
