edc9f35cb300d05856cea05fc31268b9913118ce
송제영 <wp29dud@naver.com>
2022-08-11 10:13:57 +0900

    feat : Add receiving message method

--- backend/src/main/java/com/ssafy/mbting/ws/controller/MeetingController.java
+++ backend/src/main/java/com/ssafy/mbting/ws/controller/MeetingController.java
@@ -1,18 +1,18 @@
 package com.ssafy.mbting.ws.controller;
 
 import com.ssafy.mbting.api.service.OpenviduService;
-import com.ssafy.mbting.db.enums.Gender;
-import com.ssafy.mbting.ws.model.res.TestResponse;
-import com.ssafy.mbting.ws.model.stompMessageBody.msg.RequestProposal;
+import com.ssafy.mbting.ws.model.vo.StompUser;
+import com.ssafy.mbting.ws.model.vo.WsDestination;
+import com.ssafy.mbting.ws.service.WaitingMeetingService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -22,14 +22,16 @@ public class MeetingController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final OpenviduService openviduService ;
     private final SimpMessagingTemplate simpMessagingTemplate;
-    @MessageMapping("/indi/{email}")
-    @SendTo("/ws/sub/indi/{email}")
-    public void TestMessage(@DestinationVariable("email") String email ,@Payload Message<RequestProposal> proMessage) {
-        logger.debug("\n\nproMessage: {}\n",proMessage);
-        logger.debug("\n\npayload: {}\n", proMessage.getPayload());
-        logger.debug("\n\ncommand: {}\n", proMessage.getPayload().getCommand());
-//        System.out.println("\n\n\n\n\n\n\n 들어옴 !!!!!!!!!!!"+result.toString());
-        String token =openviduService.getToken("abc");
-        simpMessagingTemplate.convertAndSend("/ws/sub/indi/"+email, TestResponse.builder().gender(Gender.MALE).command("accept").mbti("ESTP").token(token).build());
+    private final WaitingMeetingService waitingMeetingService;
+
+    @MessageMapping("/indi/proposalResult")
+    public void receiveProposalResult(@Payload Message<String> message) {
+        StompHeaderAccessor header = StompHeaderAccessor.wrap(message);
+        String sessionId = header.getSessionId();
+        StompUser stompUser = waitingMeetingService.getStompUserBySessionId(sessionId);
+        String email = stompUser.getEmail();
+        simpMessagingTemplate.convertAndSend(
+                WsDestination.of(email).getDestination(),
+                "{\"command\":\"accept\",\"data\":{\"token\":\"someTokTok\"}}");
     }
 }
