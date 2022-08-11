77875d406a2c5b1da094ec195fcd519adbfb0a46
송제영 <wp29dud@naver.com>
2022-08-11 15:53:07 +0900

    fix : WsDestination to IndividualDestination

--- backend/src/main/java/com/ssafy/mbting/ws/controller/MeetingController.java
+++ backend/src/main/java/com/ssafy/mbting/ws/controller/MeetingController.java
@@ -1,12 +1,14 @@
 package com.ssafy.mbting.ws.controller;
 
 import com.ssafy.mbting.api.service.OpenviduService;
+import com.ssafy.mbting.ws.model.event.ProposalResultArriveEvent;
 import com.ssafy.mbting.ws.model.vo.StompUser;
 import com.ssafy.mbting.ws.model.vo.IndividualDestination;
 import com.ssafy.mbting.ws.service.WaitingMeetingService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,23 +16,29 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import java.time.Clock;
+
 @Controller
 @RequiredArgsConstructor
 public class MeetingController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final OpenviduService openviduService ;
+    private final OpenviduService openviduService;
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final WaitingMeetingService waitingMeetingService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
     @MessageMapping("/indi/proposalResult")
-    public void receiveProposalResult(@Payload Message<String> message) {
+    public void receiveProposalResult(@Payload Message<Boolean> message) {
         StompHeaderAccessor header = StompHeaderAccessor.wrap(message);
         String sessionId = header.getSessionId();
         StompUser stompUser = waitingMeetingService.getStompUserBySessionId(sessionId);
         String email = stompUser.getEmail();
-        simpMessagingTemplate.convertAndSend(
-                IndividualDestination.of(email).toString(),
-                "{\"command\":\"accept\",\"data\":{\"token\":\"someTokTok\"}}");
+        applicationEventPublisher.publishEvent(new ProposalResultArriveEvent(
+                this,
+                Clock.systemDefaultZone(),
+                email,
+                message.getPayload()));
     }
 
     @MessageMapping("/indi/rejoin")
@@ -40,9 +48,10 @@ public class MeetingController {
         StompUser stompUser = waitingMeetingService.getStompUserBySessionId(sessionId);
         String email = stompUser.getEmail();
         simpMessagingTemplate.convertAndSend(
-                WsDestination.of(email).getDestination(),
+                IndividualDestination.of(email).toString(),
                 "{\"command\":\"test\",\"data\":{\"token\":\"someTokTok\"}}");
     }
+
     @MessageMapping("/indi/meetingAudioStarted")
     public void receiveAudioStarted(@Payload Message<String> message) {
         StompHeaderAccessor header = StompHeaderAccessor.wrap(message);
@@ -50,7 +59,7 @@ public class MeetingController {
         StompUser stompUser = waitingMeetingService.getStompUserBySessionId(sessionId);
         String email = stompUser.getEmail();
         simpMessagingTemplate.convertAndSend(
-                WsDestination.of(email).getDestination(),
+                IndividualDestination.of(email).toString(),
                 "{\"command\":\"test\",\"data\":{\"token\":\"someTokTok\"}}");
     }
 
@@ -62,7 +71,7 @@ public class MeetingController {
         StompUser stompUser = waitingMeetingService.getStompUserBySessionId(sessionId);
         String email = stompUser.getEmail();
         simpMessagingTemplate.convertAndSend(
-                WsDestination.of(email).getDestination(),
+                IndividualDestination.of(email).toString(),
                 "{\"command\":\"test\",\"data\":{\"token\":\"someTokTok\"}}");
     }
 
@@ -74,7 +83,7 @@ public class MeetingController {
         String email = stompUser.getEmail();
 
         simpMessagingTemplate.convertAndSend(
-                WsDestination.of(email).getDestination(),
+                IndividualDestination.of(email).toString(),
                 "{\"command\":\"test\",\"data\":{\"token\":\"someTokTok\"}}");
     }
 }
