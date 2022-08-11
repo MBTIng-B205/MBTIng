f4f524339bc946d42e66f5bd3cce6b1d43888436
송제영 <wp29dud@naver.com>
2022-08-11 10:13:16 +0900

    misc : Move BaseMessageBody

--- backend/src/main/java/com/ssafy/mbting/ws/eventListener/waitingMeetingEventListener.java
+++ backend/src/main/java/com/ssafy/mbting/ws/eventListener/waitingMeetingEventListener.java
@@ -4,7 +4,7 @@ import com.ssafy.mbting.ws.model.event.RequestToJoinQueueEvent;
 import com.ssafy.mbting.ws.model.event.WaitingMeetingUserMatchedEvent;
 import com.ssafy.mbting.ws.model.event.WaitingMeetingUserQueueSizeEnoughEvent;
 import com.ssafy.mbting.ws.model.event.WaitingMeetingUserQueuedEvent;
-import com.ssafy.mbting.ws.model.stompMessageBody.BaseMessageBody;
+import com.ssafy.mbting.ws.model.stompMessageBody.msg.BaseMessageBody;
 import com.ssafy.mbting.ws.model.vo.MeetingUser;
 import com.ssafy.mbting.ws.model.vo.WsDestination;
 import com.ssafy.mbting.ws.service.WaitingMeetingService;
