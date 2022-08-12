904f95ac01b84ad73be9ee3634b2855752f0cf2f
송제영 <wp29dud@naver.com>
2022-08-12 20:26:56 +0900

    misc : misc

--- backend/src/main/java/com/ssafy/mbting/ws/service/WaitingMeetingServiceImpl.java
+++ backend/src/main/java/com/ssafy/mbting/ws/service/WaitingMeetingServiceImpl.java
@@ -3,7 +3,7 @@ package com.ssafy.mbting.ws.service;
 import com.ssafy.mbting.api.service.MemberService;
 import com.ssafy.mbting.api.service.OpenviduService;
 import com.ssafy.mbting.ws.model.vo.MeetingRoom;
-import com.ssafy.mbting.ws.repository.StompRepository;
+import com.ssafy.mbting.ws.repository.AppRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,32 +18,32 @@ public class WaitingMeetingServiceImpl implements WaitingMeetingService {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final ApplicationEventPublisher applicationEventPublisher;
-    private final StompRepository stompRepository;
+    private final AppRepository appRepository;
     private final MemberService memberService;
     private final OpenviduService openviduService;
 
     @Override
     public int getQueueSize() {
-        return stompRepository.getQueueSize();
+        return appRepository.getQueueSize();
     }
 
     @Override
     public String getFirstSessionId() {
         logger.debug("\n\n대기열의 맨 앞 꺼내기 시도...\n");
-        String sessionId = stompRepository.getFirstSessionId()
+        String sessionId = appRepository.getFirstSessionId()
                 .orElseThrow(() -> {
                     logger.error("\n\n!!! No Element !!!\n대기열이 비어 있음\n");
                     return new RuntimeException("Internal Server Error!");
                 });
-        stompRepository.leaveFromQueue(sessionId);
+        appRepository.leaveFromQueue(sessionId);
         logger.debug("\n\n잘 꺼냄\nsessionId: {}\n", sessionId);
         return sessionId;
     }
 
     @Override
     public void setMatchedMeetingUsers(String sessionId1, String sessionId2) {
-        stompRepository.setMatchedMeetingUser(sessionId1, sessionId2);
-        stompRepository.setMatchedMeetingUser(sessionId2, sessionId1);
+        appRepository.setMatchedMeetingUser(sessionId1, sessionId2);
+        appRepository.setMatchedMeetingUser(sessionId2, sessionId1);
     }
 
     @Override
@@ -56,21 +56,21 @@ public class WaitingMeetingServiceImpl implements WaitingMeetingService {
                 openviduService.getToken(openviduSessionName)};
 
 
-        stompRepository.saveMeetingRoom(meetingRoomId,
+        appRepository.saveMeetingRoom(meetingRoomId,
                 MeetingRoom.newMeetingRoom(
                         openviduSessionName,
                         sessionId1,
                         sessionId2,
-                        stompRepository.findStompUserBySessionId(sessionId1)
+                        appRepository.findStompUserBySessionId(sessionId1)
                                 .orElseThrow(() -> new RuntimeException("Session Not Found!"))
                                 .getMeetingUser().getMbti(),
-                        stompRepository.findStompUserBySessionId(sessionId2)
+                        appRepository.findStompUserBySessionId(sessionId2)
                                 .orElseThrow(() -> new RuntimeException("Session Not Found!"))
                                 .getMeetingUser().getMbti()
                 ));
 
-        stompRepository.setMeetingRoomIdAndIndex(sessionId1, meetingRoomId, 0);
-        stompRepository.setMeetingRoomIdAndIndex(sessionId2, meetingRoomId, 1);
+        appRepository.setMeetingRoomIdAndIndex(sessionId1, meetingRoomId, 0);
+        appRepository.setMeetingRoomIdAndIndex(sessionId2, meetingRoomId, 1);
         return tokens;
     }
 }
