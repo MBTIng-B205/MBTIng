e1eb7fa4b78fdc3fb86594bef6905cb23905ae52
lgj2022 <98953739+lgj2022@users.noreply.github.com>
2022-08-18 09:56:44 +0900

    feat : interest tag

--- backend/src/main/java/com/ssafy/mbting/ws/model/vo/MeetingUser.java
+++ backend/src/main/java/com/ssafy/mbting/ws/model/vo/MeetingUser.java
@@ -20,11 +20,20 @@ public class MeetingUser {
     private List<String> interests = new ArrayList<>();
 
     public static MeetingUser of(SubscribeHeader subscribeHeader) {
+        List<String> interests = new ArrayList<>();
+        String interest = String.valueOf(subscribeHeader.getInterests());
+        interest = interest.replace("[","");
+        interest = interest.replace("]","");
+        interest = interest.replaceAll("\"", "");
+        String [] interestsarr = interest.split(",");
+        for (String i : interestsarr){
+            interests.add(i);
+        }
         return MeetingUser.builder()
                 .mbti(subscribeHeader.getMbti())
                 .gender(Gender.valueOf(subscribeHeader.getGender().trim().toUpperCase()))
                 .sido(subscribeHeader.getSido())
-                .interests(subscribeHeader.getInterests())
+                .interests(interests)
                 .build();
     }
 }
